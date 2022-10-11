940ba6acbe4986ca658e59d166a47f45fe5549d1
Rawa <rawa.murat@gmail.com>
2022-10-10 23:45:56 -0400

    add parameters to mavn test

---
+++ src/test/java/testcases/SampleTestClass.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTestClass {
+
+	@Test
+	public void sampleTestcase()
+	{
+		System.out.println("Test 1 executed");
+	}
+	
+}
---
+++ src/test/java/testcases/SampleTestClass2.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.testng.annotations.Test;
+
+public class SampleTestClass2 {
+
+	@Test
+	public void sampleTestcase()
+	{
+		System.out.println("Test 2 executed");
+	}
+	
+}
