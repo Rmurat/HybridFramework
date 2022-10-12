1c3e818781b02947fcbe767965c1834d3c935aa0
Rawa <rawa.murat@gmail.com>
2022-10-11 23:29:30 -0400

    new commit

--- src/main/java/base/BaseClass.java
+++ src/main/java/base/BaseClass.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 import browserFactory.BrowserFactory;
 import dataProviders.ConfigUtility;
@@ -15,13 +16,25 @@ public class BaseClass {
 
 	public static WebDriver driver;
 
+	@Parameters({"BrowserName","AppToTest"})
 	@BeforeClass
-	public void setupBrowser()
+	public void setupBrowser(String browser, String url)
 	{
-		driver = BrowserFactory.startBrowser(ConfigUtility.getValue("browser"), ConfigUtility.getValue("appURL"));
+		/*
+		 *  This is best when you dont have to change browser and url very freq.If values are very dynamic in nature then
+		 *  we will go with second approach where user will pass this details while running
+		 * 
+		 1 -driver = BrowserFactory.startBrowser(ConfigUtility.getValue("browser"), ConfigUtility.getValue("appURL"));
+		
+		*/
+		System.out.println("Test will ne be running on "+browser +" "+url);
+		
+		driver = BrowserFactory.startBrowser(browser, url);
+
 		
 
 	}
+
 
 	@AfterClass
 	public void closeBrowser()
